e81d01c3e48dea63b80a085eb9e14bf90f98f5bf
Peter Gross <pete.gross@btinternet.com>
2020-09-04 13:20:04 +0100

    Updated start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener"
 
 const start = async () => {
-  console.log("Starting...")
+  console.log("Starting....")
   // first check that all required environment variables are defined
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined")
